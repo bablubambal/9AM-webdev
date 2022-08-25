7a21ac59c45ab658d207a545b19859e8ca21b572
B@blu1234 <bablubambal1999@gmail.com>
2022-08-25 10:45:53 +0530

    operators in js

---
+++ Js/js/02Operators.js
@@ -0,0 +1,67 @@
+console.log("Operators")
+/** Arithmatic operators */
+let a = 10;
+let b= 3;
+
+let res = a+b;
+res = a-b;
+res = a*b;
+res = a/b;
+res = a%b;
+//Increment and decrement operators
+
+// res =  a--;
+// console.log("The result is ",res,a)
+
+//js Assign operator
+
+// let d = "Hi"
+// console.log(d)
+
+//  a += b;  // a = a+b;
+//  a -= b;
+//  a *=b;
+// a /= b;  // a = a/b;
+// console.log(a)
+
+
+// Comparsion Operators
+
+let c = 23;
+let d = 23;
+console.log(typeof(c),typeof(d))
+//comparison operator 
+console.log(c==d) // values are checked
+console.log(c===d) // data types are also checked
+
+console.log("..........")
+
+// console.log(c>d)
+// console.log(c<d)
+// console.log(c<=d)
+// console.log(c>=d)
+// console.log(c != d)
+
+
+// Logical Operatos in Js 
+/***
+ * 1 And Operator
+ * 2. Or operator
+ * 3. Not operator
+ */
+
+
+a = 23;
+b = 24;
+c = 25;
+d = 26;
+
+console.log(25>24)
+console.log(25>26)
+console.log("Final and",26>24 && 27>26 )
+console.log("Final or",22>24 || 27>26 )
+
+// Btiwsie operatoer:
+
+// operatoer : 
+// & | ~ ^ << >> >>>
---
+++ Js/js/day2.js
@@ -0,0 +1,29 @@
+console.log('Day 2 Conditioal Staments  ')
+
+let rain = false;
+// console.log(rain,typeof(rain));
+
+/**CONDITIONAL Staments */
+
+if(rain){
+    console.log("It's rainging so not going to school")
+}
+else{
+console.log("Goo to School")
+}
+
+let marks = 40;
+if(marks>35){
+    console.log("student passed")
+}
+else{
+    console.log("student failed")
+}
+
+age =  18;
+if(age>=18){
+    console.log("Eligible to vote")
+}
+else{
+    console.log("You cant vote")
+}
