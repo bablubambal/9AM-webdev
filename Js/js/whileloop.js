3ca98cba25a2320593fa9b86b7f466d4545cf2e5
B@blu1234 <bablubambal1999@gmail.com>
2022-09-01 15:25:57 +0530

    while loops in Javascript

---
+++ Js/js/whileloop.js
@@ -0,0 +1,26 @@
+console.log("While Loops learning")
+
+//Why we are using while loops:
+
+// for loop while do while 
+/**
+ * Scenario
+ * whenever we know how many times i need
+ * to loop then we use for loops
+ */
+
+let i = 11;
+console.log("while loop starts")
+while (i<=10){
+    console.log(i)
+    i++
+}
+console.log("While loop ends")
+console.log("****")
+j =11 
+console.log("Do while starts")
+do {
+    console.log(j)
+    j++
+} while (j<=10);
+console.log("Do while ends")
